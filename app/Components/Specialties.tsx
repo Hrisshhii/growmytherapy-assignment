a05750363a74cc4a9071e38775965b2f8279d7f8
Hrishi <rathodhrishi12345@gmail.com>
2026-01-31 22:13:03 +0530

    Adding a basic specialties section

--- app/Components/Specialties.tsx
+++ app/Components/Specialties.tsx
@@ -18,3 +18,30 @@ const specialties = [
     image: "/specialty-3.jpg",
   },
 ];
+
+export default function Specialties(){
+  return (
+    <section className="bg-[#fbf7f2]">
+      <div className="mx-auto max-w-[1600px] px-[3.75vw]  pt-[140px] pb-[120px]">
+
+        <h2 className="mb-[96px] text-center text-[3rem] font-medium text-[#223614] mb-[80px]">
+          My Specialties
+        </h2>
+
+        <div className="grid grid-cols-1 gap-[40px] md:grid-cols-3">
+          {specialties.map((item)=>(
+            <div key={item.title} className="border border-[#223614] bg-[#e9e4dc] p-[32] flex flex-col">
+              <h3 className="text-[1.5] font-medium text-[#223614]">{item.title}</h3>
+              <p className="mt-4 text-[1rem] leading-[1.6] max-w-[420px]">{item.text}</p>
+              <div className="mt-auto pt-[48px]">
+                <div className="mx-auto w-[260px] h-[260px] rounded-full overflow-hidden">
+                  <img src={item.image} alt="" className="w-full h-full object-cover"/>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Primary from "./Components/Primary";
 import Secondary from "./Components/Secondary";
+import Specialties from "./Components/Specialties";
 
 export default function Home() {
   return (
     <main>
         <Primary />
         <Secondary />
+        <Specialties />
       <div style={{ height: "200vh" }} />
 
 
